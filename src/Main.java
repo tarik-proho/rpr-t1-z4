9337221ee80df5fe7bf0300ebd6dac01bacb7eb1
tproho1 <tproho1@etf.unsa.ba>
2018-10-18 12:45:05 +0200

    Inital commit

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+package etf.unsa.ba;
+/*
+Sve klase trebaju podrzavati
+ispis studenta na ekran u obliku PRezime IMe(brindexa) sa kodom System.out.println(s); gdje je s referenca na kalsu student
+Predmet treba ispisivati spisak studenata oblika
+1. Prezime Ime (brindexa)
+2. Prezime Ime (brindexa)
+3. Prezime Ime (brindexa)
+4. Prezime Ime (brindexa)
+
+KORISTITI NIZOVE A NE ArrayList
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+
+
+
+public class Student{
+    String ime;
+    String prezime;
+    int brIndeksa;
+}
+public class Predmet{
+    //privatni atribut niz studenata
+    //metode upis i ispis koje primaju referencu na studenta
+    //podaci o predmetu su naziv predmeta šifra predmeta i maksimalan broj studenata
+    //podaci o studentu su ime prezime i broj indeksa (zadavanje pomoću konstruktora)
+    //podaci o studentu se trebaju mijenjati i pomocu gettera i settera osim maks broja studenata na predmetu koji je fiksan
+
+}
+
+
+
+
+public class Program{
+
+    public static void main(String[] args) {
+        //Kreiranje novog studenta
+        //kreiranje novog studenta
+        //upis studenta na predmet
+        //ispis studenta sa predmeta
+        //brisanje studenta
+        //ispis spiska studenata na predmetu
+    }
+}
